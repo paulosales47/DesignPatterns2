b3657dd446fb1a9e585bbc09699b956a91a899f7
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-02-13 17:55:09 -0200

    Add project files.

---
+++ DesignPatterns2/Aula01/ConnectionFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2.Aula01
+{
+    public class ConnectionFactory
+    {
+        public IDbConnection GetConnection()
+        {
+            IDbConnection conexao = new SqlConnection();
+            conexao.ConnectionString = "User Id=root;Password=;Server=localhost;Database=banco";
+            conexao.Open();
+            return conexao;
+        }
+    }
+}
---
+++ DesignPatterns2/Program.cs
@@ -0,0 +1,18 @@
+﻿using DesignPatterns2.Aula01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conexao = new ConnectionFactory();
+            conexao.GetConnection();
+        }
+    }
+}
